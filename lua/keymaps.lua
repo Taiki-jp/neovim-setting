45aca1febbdae595ad9b92803ca35daf39a4f5d8
taikisenju <senju.taiki@laiblitz.co.jp>
2024-04-03 20:38:12 +0900

    :tada: Neovim settings

---
+++ lua/keymaps.lua
@@ -0,0 +1,205 @@
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+
+--local keymap = vim.keymap
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = 'n',
+--   insert_mode = 'i',
+--   visual_mode = 'v',
+--   visual_block_mode = 'x',
+--   term_mode = 't',
+--   command_mode = 'c',
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- New tab
+keymap("n", "te", ":tabedit", opts)
+-- 新しいタブを一番右に作る
+keymap("n", "gn", ":tabnew<Return>", opts)
+-- move tab
+keymap("n", "gh", "gT", opts)
+keymap("n", "gl", "gt", opts)
+
+-- Split window
+keymap("n", "ss", ":split<Return><C-w>w", opts)
+keymap("n", "sv", ":vsplit<Return><C-w>w", opts)
+
+-- Select all
+keymap("n", "<C-a>", "gg<S-v>G", opts)
+
+-- Do not yank with x
+keymap("n", "x", '"_x', opts)
+
+-- Delete a word backwards
+keymap("n", "dw", 'vb"_d', opts)
+
+-- 行の端に行く
+keymap("n", "<Space>h", "^", opts)
+keymap("n", "<Space>l", "$", opts)
+
+-- ;でコマンド入力( ;と:を入れ替)
+keymap("n", ";", ":", opts)
+
+-- 行末までのヤンクにする
+keymap("n", "Y", "y$", opts)
+
+-- <Space>q で強制終了
+keymap("n", "<Space>q", ":q!<Return>", opts)
+
+-- save file
+keymap("n", "<Space>w", ":w<Return>", opts)
+
+-- ESC*2 でハイライトやめる
+keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", opts)
+
+-- ll でluaファイルを読み込む
+keymap("n", "<Space>l", ":luafile %<CR>", opts)
+
+-- Insert --
+-- Press jk fast to exit insert mode
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "jj", "<ESC>", opts)
+
+-- コンマの後に自動的にスペースを挿入
+keymap("i", ",", ",<Space>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- ビジュアルモード時vで行末まで選択
+keymap("v", "v", "$h", opts)
+
+-- 0番レジスタを使いやすくした
+keymap("v", "<C-p>", '"0p', opts)
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+-- refering nvim-lspconfig README
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+-- vim.keymap.set('n',  '<space>q',  vim.diagnostic.setloclist)
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+-- Find files using Telescope command-line sugar.
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- vim-pry
+-- local pry = require('vim-pry')
+-- vim.keymap.set('n', '<leader>d',  pry.insert,  {})
+-- nvim-tree
+keymap("n", "<C-f>", ":NvimTreeToggle<Return>", opts)
+
+-- vim-rails
+vim.keymap.set('n', '<leader>b', ':bp<CR>', opts)
+vim.keymap.set('n', '<leader>n', ':bn<CR>', opts)
+
+-- Execute current file
+function execute_current_file()
+  local file = vim.api.nvim_buf_get_name(0)
+  local file_extension = file:match("^.+(%..+)$")
+
+  if file_extension == ".py" then
+    vim.cmd("!python3 " .. file)
+  elseif file_extension == ".fish" then
+    vim.cmd("!fish " .. file)
+  elseif file_extension == ".lua" then
+    vim.cmd("luafile " .. file)
+  else
+    print("File type not supported for execution")
+  end
+  print("Executed")
+end
+
+vim.keymap.set('n', '<leader><leader>', ':lua execute_current_file()<CR>')
+
+-- gitsigns recommended keymap
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    -- Actions
+    map('n', '<leader>hs', gs.stage_hunk)
+    map('n', '<leader>hr', gs.reset_hunk)
+    map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('n', '<leader>hS', gs.stage_buffer)
+    map('n', '<leader>hu', gs.undo_stage_hunk)
+    map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>hp', gs.preview_hunk)
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    map('n', '<leader>hd', gs.diffthis)
+    map('n', '<leader>hD', function() gs.diffthis('~') end)
+    map('n', '<leader>td', gs.toggle_deleted)
+
+    -- Text object
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
